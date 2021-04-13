dd14e3df0eb10b08c329aa783e903ebfe282fc38
unknown <zim21den21@gmail.com>
2021-04-13 16:09:13 +0300

    img js

--- js/img.js
+++ js/img.js
@@ -4,5 +4,5 @@ let big_img = document.querySelector('.big-img img');
 education.addEventListener('click', e => {
     if (e.target.tagName !== 'IMG') return;
     const img = e.target.dataset.img;
-    big_img.src = `../video-images/certificate/${img}.png`;
+    big_img.src = `../video-images/certificates/${img}.png`;
 });
